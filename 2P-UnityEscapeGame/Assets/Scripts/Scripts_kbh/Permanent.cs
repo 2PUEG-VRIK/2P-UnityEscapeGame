ca7dea9556c1f8130fd2babd0e9797cb2e8ac731
qhgms <55770849+qhgms@users.noreply.github.com>
2021-09-28 17:45:46 +0900

    0928-1745
    개인 스크립트 작성

--- 2P-UnityEscapeGame/Assets/Scripts/Scripts_kbh/Permanent.cs
+++ 2P-UnityEscapeGame/Assets/Scripts/Scripts_kbh/Permanent.cs
@@ -4,48 +4,88 @@ using UnityEngine;
 
 public class Permanent : MonoBehaviour
 {
- //   public int number;//몬스터 수
-    public GameObject[] m;//몬스터 배열
-    public Transform Rise;//부모(뭉텅이)
-    public Transform[] Children;//부모 속 각 객체들
+    public GameObject Parent;//올리는 애들(뭉텅이)
+    //Transform[] Children;//부모 속 각 객체들
     
-    GameObject wall7;
-    Transform target1;//떨굴 벽 위치정보
+    GameObject[] m;//몬스터 배열
+
+    GameObject wall7;//떨굴 벽
     //GameObject[] allChildren;
 
+    int check = -1;
 
-    private void Start()
+    
+    private void Awake()
     {
         wall7 = GameObject.Find("Wall (7)");
-        target1 = wall7.GetComponent<Transform>();
-        Children = Rise.gameObject.GetComponentsInChildren<Transform>();
-        
+        //Children = Parent.gameObject.GetComponentsInChildren<Transform>();
+       // Parent = GameObject.FindWithTag("rise");
+       
+
     }
     private void FixedUpdate()
     {
         m = GameObject.FindGameObjectsWithTag("Enemy");
-        //      number = m.Length;
-        if (m.Length == 0)
+        
+    }
+    private void Update()
+    {
+        if(check==-1) Co();
+        // if (m.Length ==0 && check==-1) RiseUp();
+
+
+        if (wall7.transform.position.y <= -60)//|| Parent.transform.position.y > -2
         {
-
-            wall7.transform.Translate(Vector3.down, Space.Self);
-
-            while (Rise.position.y != 3)
-                Rise.Translate(Vector3.up, Space.Self);
-
-            //foreach(Transform child in Rise)
-            // {
-            //     if(child.name==transform.name)
-            //         child.position = new Vector3(102.39f, 3, -23.150f);
-
-            // }
-
-            if (target1.position.y == -90)
-                Destroy(wall7);
-
+            //wall7.transform.position = new Vector3(52.8f, -50, 0);
+            wall7.SetActive(false);
+            StopCoroutine("Falldown");
 
         }
 
-    }
+        //if (Parent.transform.position.y >= -2)
+        //{
+        //    check--;
+        //    Debug.Log("라이즈 끝내자~~~~~~~`");
 
+
+        //    StopCoroutine("RiseUpFunc");
+
+        //}
+
+    }
+    void Co()//코루틴 실행 함수
+    {
+        StartCoroutine("Falldown");
+       // StartCoroutine("RiseUpFunc");
+        
+         
+
+    }
+    IEnumerator Falldown()//벽 떨구는 함수
+    {
+        wall7.transform.Translate(Vector3.down, Space.Self);
+        yield return null;
+    }
+   
+    //IEnumerator RiseUpFunc()
+    //{
+
+    //    //Parent.transform.Translate(Vector3.up.normalized);
+
+    //    //foreach (Transform child in Parent)
+    //    //{
+    //    //    rigid.AddForce(new Vector3(0, -1, 0));
+    //    //    //child.transform.Translate(new Vector3(0,0.5f,0));
+    //    //    if (child.transform.position.y > 0)
+    //    //     
+
+    //    //}
+    //    Parent.transform.position += Vector3.up * 0.3f;
+
+       
+    //    yield return StartCoroutine(Falldown());
+    //}
+        
+
+       
 }
